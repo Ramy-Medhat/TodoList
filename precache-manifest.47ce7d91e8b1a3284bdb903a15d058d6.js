99c1bd3c189791aa8ba1ebc92a5b4292a2c45926
Ramy Medhat <ramy.med7t@gmail.com>
2019-02-27 11:32:49 +0200

    Updates

---
+++ precache-manifest.47ce7d91e8b1a3284bdb903a15d058d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ramy-Medhat/TodoList/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6dbc3e963b7bc863c9af",
+    "url": "/Ramy-Medhat/TodoList/static/js/runtime~main.6dbc3e96.js"
+  },
+  {
+    "revision": "f741e6694f126d8b41cf",
+    "url": "/Ramy-Medhat/TodoList/static/js/main.f741e669.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Ramy-Medhat/TodoList/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f741e6694f126d8b41cf",
+    "url": "/Ramy-Medhat/TodoList/static/css/main.660ccb82.chunk.css"
+  },
+  {
+    "revision": "f0be169632a3d0dafb32c02c50d9ef2c",
+    "url": "/Ramy-Medhat/TodoList/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b05344fa788a0274c286f9dcf21ef83.js"
+  "/Ramy-Medhat/TodoList/precache-manifest.47ce7d91e8b1a3284bdb903a15d058d6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Ramy-Medhat/TodoList/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
